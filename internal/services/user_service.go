2d214b899ec2e06e21c064d2f0a7d74d145162b8
dagulv <dagulvsback@gmail.com>
2023-07-02 15:50:20 +0200

    Added service abstraction to fetch from store

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -6,17 +6,45 @@ import (
 
 	"github.com/dagulv/stock-api/internal/models"
 	"github.com/dagulv/stock-api/internal/stores"
+	"github.com/go-playground/validator/v10"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/rs/xid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
-	store stores.UserStore
+	Store    stores.UserStore
+	Validate *validator.Validate
 }
 
-func (s UserService) Post(ctx context.Context, user *models.User) (err error) {
+func (s UserService) List(ctx context.Context, set func(*models.User) error) (err error) {
+	return s.Store.List(ctx, set)
+}
+
+func (s UserService) Create(ctx context.Context, user *models.User) (err error) {
 	user.Id = xid.NewWithTime(time.Now())
-	user.TimeCreated = pgtype.Timestamptz{Time: time.Now()}
+	user.TimeCreated = pgtype.Timestamptz{Time: time.Now(), Valid: true}
 	user.TimeUpdated = user.TimeCreated
 
+	s.Store.Create(ctx, user)
+
+	return
+}
+
+func (s UserService) Get(ctx context.Context, userId xid.ID, dst *models.User) error {
+	return s.Store.Get(ctx, userId, dst)
+}
+
+func (s UserService) GetByEmail(ctx context.Context, email string, dst *models.User) error {
+	return s.Store.GetByEmail(ctx, email, dst)
+}
+
+func (s UserService) SetPassword(ctx context.Context, password string, userId xid.ID) (err error) {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+
+	if err != nil {
+		return
+	}
+
+	return s.Store.SetPassword(ctx, string(passwordHash[:]), userId)
 }
--- internal/stores/user_store.go
+++ internal/stores/user_store.go
@@ -4,8 +4,13 @@ import (
 	"context"
 
 	"github.com/dagulv/stock-api/internal/models"
+	"github.com/rs/xid"
 )
 
 type UserStore interface {
-	Post(ctx context.Context, user *models.User) error
+	Create(ctx context.Context, user *models.User) error
+	SetPassword(ctx context.Context, passwordHash string, userId xid.ID) error
+	GetByEmail(ctx context.Context, email string, dst *models.User) error
+	List(ctx context.Context, set func(*models.User) error) error
+	Get(ctx context.Context, userId xid.ID, dst *models.User) error
 }
